1072562b0da04493ba391615c63842e7b7e3f485
alex <alex.sbox24@gmail.com>
2020-03-02 22:55:45 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)){
+    if (array.length != 0){
+      return array.reduce(function (prev, item){
+        return prev < item ? prev : item; 
+      },0);
+    }
+    else {
+      return 0;
+    }
+
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)){
+    if (array.length != 0){
+      return array.reduce(function (prev, item){
+        return prev > item ? prev : item; 
+      },0);
+    }
+    else {
+      return 0;
+    }
+
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array)){
+    if (array.length != 0){
+      return array.reduce(function (prev, item){
+        return prev + item; 
+      },0) / array.length;
+    }
+    else {
+      return 0;
+    }
+
+  }
+  else {
+    return 0;
+  }
 }
